49e469dd0f4be3d6489e65a21a4a250973c528a9
kanta <kanta.sato0926@gmail.com>
2025-06-23 07:53:44 +0900

    page-修正

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -5,9 +5,6 @@ import { useEffect } from "react";
 export default function AdBanner() {
   useEffect(() => {
     try {
-      // @ts-expect-error 宣言が必要な理由
-      /* adsbygoogleの型チェック回避のため */
-      //   asobygogleを初期化、広告挿入
       (window.adsbygoogle = window.adsbygoogle || []).push({});
     } catch (e) {
       console.error("Adsense error", e);
